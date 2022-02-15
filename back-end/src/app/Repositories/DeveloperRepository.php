08070213524060ca0509ae3aac09e099fea63d3b
Alessandro Frias <alessandroribeirofrias@outlook.com>
2022-02-14 23:23:44 -0300

    feat(produto): [EC-1615] - Tests of commit

--- back-end/src/app/Repositories/DeveloperRepository.php
+++ back-end/src/app/Repositories/DeveloperRepository.php
@@ -44,21 +44,18 @@ class DeveloperRepository {
         return $developerChange->save();
    }
 
-     public function destroy($developerDelete, $id)
-     {
-          return $developerDelete->delete($id);
-     }
+   public function destroy($developerDelete, $id)
+   {
+        return $developerDelete->delete($id);
+   }
 
-   public function novoDeveloper($dados)
+   public function store($dados)
    {
         $this->developer->nome = $dados->nome;
         $this->developer->sexo = $dados->sexo;
         $this->developer->idade = $dados->idade;
         $this->developer->hobby = $dados->hobby;
         $this->developer->datanascimento = $dados->datanascimento;
-        //comentario
-
-        $codigoo = 'teste';
         return $this->developer->save();
    }
 }
